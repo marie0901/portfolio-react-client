b663b31cee3b6eb9694007ecee11a8c3ba0e7d50
Maliarets <MMaliarets@DBDC.luxoft.com>
2018-09-22 12:53:29 +0200

    add token

--- src/data/constants.js
+++ src/data/constants.js
@@ -6,7 +6,7 @@ export const DOMAIN_NAME = 'http://127.0.0.1:8000';
 export const EMAIL = 'marie0901@gmail.com';
 
 // Replace this with authentication token generated from your Django admin dashboard
-export const API_AUTH_TOKEN = '	f4881869bbc57a34e24f0449ec8ae6aa7de90402';
+export const API_AUTH_TOKEN = '3075868b0fad4dbe028d14fb093766c6ca71cca9';
 
 // Replace this with your Google Analytics tracking number
 export const GA_TRACKING_NUMBER = 'UA-107377138-1';
